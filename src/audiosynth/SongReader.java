6bf9495c98473512555ffbf39f909153e98c08f2
alisonlange45 <alange@macalester.edu>
2020-10-15 11:39:03 -0500

    cleaned up code

--- src/audiosynth/SongReader.java
+++ src/audiosynth/SongReader.java
@@ -41,13 +41,6 @@ public class SongReader{
         waveforMap.put("square", new SquareWave());
         waveforMap.put("saw", new SawtoothWave());
         waveforMap.put("noise", new Noise());
-        // Hint: use a Map instance variable to track the name → waveform mappings.
-        //
-        // Later you will get waveforms out of this map and use them to create Notes.
-        //
-        // Look at the `AudioSynth.main()` test code from the previous step
-        // for a reminder of how to make an object that represents a waveform
-        // of a particular type.
     }
     /**
      * Reads song data from the given project resource.
@@ -79,16 +72,6 @@ public class SongReader{
             double pitch = scanner.nextDouble();
             double startTime = scanner.nextDouble();
             double duration = scanner.nextDouble();
-            // Use scanner.next() and scanner.nextDouble() to read the following
-            // things, in this order:
-            //
-            // - waveform name
-            // - pitch
-            // - start time
-            // - duration
-            //
-            // ...then use that data to create a new Note and add it to the song.
-            // (Look at the Note constructor to see what types you need in what order.)
             Note note = new Note(waveform, pitch, startTime, duration);
             song.addNote(note);
         }
